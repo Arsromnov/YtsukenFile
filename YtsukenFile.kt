815a15149dbd495575d40ac1c7f287808887a086
Арсений <ytsyken520@gmail.com>
2026-04-22 20:35:24 +0300

    Добавил основной файл

---
+++ YtsukenFile.kt
@@ -0,0 +1,37 @@
+import java.io.File
+
+fun main() {
+
+
+    println("Ytsuken/File [1.0.0] ")
+    println("Enter <filename>.<extension>")
+    println()
+
+    while (true) {
+
+        val File = readln().trim()
+        val Invalid = "\\/:*?\"<>|"
+
+
+        if (File.isEmpty()) {
+            println("File name cannot be empty")
+            continue
+        }
+
+        if (File.any { it in Invalid }) {
+            println("The file name contains prohibited characters")
+            continue
+        }
+
+
+        val New = File(File)
+
+        if (New.exists()) {
+            println("The file already exists")
+            continue
+        }
+
+        New.createNewFile()
+
+    }
+}
